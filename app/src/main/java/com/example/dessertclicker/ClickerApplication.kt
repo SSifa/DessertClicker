22f915f4886a0f93853750df8f7c15b66a16cfc6
SSifa <stivengombo@gmail.com>
2023-08-04 11:47:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dessertclicker/ClickerApplication.kt
@@ -0,0 +1,12 @@
+package com.example.dessertclicker
+
+import android.app.Application
+import timber.log.Timber
+
+class ClickerApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
+}
